b47bb34ca9828f477153c616028332d8eb71317e
Adolligit <adelson.onod@gmail.com>
2022-02-01 15:15:17 -0300

    nenhuma modificação siginificativa

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <p>TrybeTunes</p>
+        <p>Outro pull para presença</p>
+      </>
+    );
   }
 }
 
