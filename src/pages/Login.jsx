40572f1b7087539546a79dd4c97b06fb3b9519f2
Adolligit <adelson.onod@gmail.com>
2022-02-23 13:54:56 -0300

    Adicionado formulário e redirect para 'Serach'

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,77 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
+import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      offBtn: true,
+      loading: false,
+      redirect: undefined,
+    };
+
+    this.changeName = this.changeName.bind(this);
+    this.saveName = this.saveName.bind(this);
+  }
+
+  returnForm = () => {
+    const { name, offBtn } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <form>
+        <label htmlFor="name">
+          Nome:
+          <input
+            data-testid="login-name-input"
+            type="text"
+            value={ name }
+            onChange={ this.changeName }
+          />
+        </label>
+        <button
+          data-testid="login-submit-button"
+          type="button"
+          disabled={ offBtn }
+          onClick={ () => this.saveName(name) }
+        >
+          Entrar
+        </button>
+      </form>
+    );
+  }
+
+  disabledButton() {
+    const minimumValue = 3;
+    const { name } = this.state;
+
+    this.setState({ offBtn: (name.length < minimumValue) ? 1 : 0 });
+  }
+
+  changeName({ target: { value } }) {
+    this.setState({ name: value }, this.disabledButton);
+  }
+
+  async saveName(name) {
+    this.setState({ loading: true });
+    createUser({ name })
+      .then(() => this.setState({
+        loading: false,
+        redirect: <Redirect to="/search" />,
+      }));
+  }
+
+  render() {
+    const { loading, redirect } = this.state;
+    return (
+      <div data-testid="page-login">
+        <h1>Login</h1>
+        {
+          (loading) ? <Loading /> : this.returnForm()
+        }
+        { redirect }
+      </div>
     );
   }
 }
