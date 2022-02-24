7be3cf8cdc8426d6c53dc809e08d9fa4418c9225
Adolligit <adelson.onod@gmail.com>
2022-02-24 10:27:49 -0300

    Campo de pesquisa adicionado a página 'Search'

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,47 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searching: '',
+    };
+
+    this.changeSearching = this.changeSearching.bind(this);
+    this.disabledButton = this.disabledButton.bind(this);
+  }
+
+  changeSearching({ target: { value } }) {
+    this.setState({ searching: value });
+  }
+
+  disabledButton() {
+    const { searching } = this.state;
+
+    return (searching.length < 2) ? 1 : 0;
+  }
+
   render() {
+    const { searching } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            name="searching"
+            data-testid="search-artist-input"
+            value={ searching }
+            onChange={ this.changeSearching }
+          />
+          <input
+            type="button"
+            value="Pesquisar"
+            data-testid="search-artist-button"
+            disabled={ this.disabledButton() }
+          />
+        </form>
       </div>
     );
   }
