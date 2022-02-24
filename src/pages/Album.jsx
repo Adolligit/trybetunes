d67d31b1a6ab9b50872757ed38ac898ebe152e47
Adolligit <adelson.onod@gmail.com>
2022-02-24 18:02:14 -0300

    Adicionado o nome do author e listada suas músicas

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from './Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      author: '',
+      musics: [],
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+
+    getMusics(id)
+      .then((response) => {
+        response.shift();
+        this.setState({
+          author: response[0].artistName,
+          musics: response,
+        });
+      });
+  }
+
   render() {
+    const { author, musics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <fieldset>
+          <h1 data-testid="artist-name">{ author }</h1>
+          <ul>
+            {
+              musics.map(({ trackName }) => (
+                <li key={ trackName }>{ trackName }</li>
+              ))
+            }
+          </ul>
+        </fieldset>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.objectOf(),
+}.isRequired;
 
 export default Album;
